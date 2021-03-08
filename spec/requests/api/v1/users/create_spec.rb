97f42fe0c34c7a110028000867c2d78d4f13e47b
Max Ribbans <ribbans.max@gmail.com>
2021-03-08 16:39:00 -0500

    Sad path for user create

--- spec/requests/api/v1/users/create_spec.rb
+++ spec/requests/api/v1/users/create_spec.rb
@@ -15,4 +15,34 @@ describe "Creates a user" do
       expect(User.last.email).to eq(data[:email])
     end
   end
+
+  describe 'sad' do
+    it "should not create a user if email already exists" do
+      User.create!(:email => 'email@example.com', password: 'password', password_confirmation: 'password')
+
+      expect(User.last.email).to eq('email@example.com')
+
+      data = {
+        email: 'email@example.com',
+        password: 'Password',
+        password_confirmation: 'Password'
+      }
+      headers = { "CONTENT_TYPE" => "application/json" }
+      post '/api/v1/users', params: data
+
+      expect(response.status).to eq(400)
+    end
+
+    it 'should not create a user if their password does not match the confirmation' do
+      data = {
+        email: 'email@example.com',
+        password: 'Password',
+        password_confirmation: 'password'
+      }
+      headers = { "CONTENT_TYPE" => "application/json" }
+      post '/api/v1/users', params: data
+
+      expect(response.status).to eq(400)
+    end
+  end
 end
