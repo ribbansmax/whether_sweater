871794e58d7977e877880ae86ec7e1ef3ca708ed
Max Ribbans <ribbans.max@gmail.com>
2021-03-07 21:49:56 -0500

    Generate api-key for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
+  before_create :set_api
   validates :email, presence: true, uniqueness: true
   validates_presence_of :password, required: true
   has_secure_password
+
+  def set_api
+    self.api_key = SecureRandom.uuid
+  end
 end
