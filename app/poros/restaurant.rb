74c8845fc171e09bd3cd61d2488710429d53ed89
Max Ribbans <ribbans.max@gmail.com>
2021-03-08 12:39:13 -0500

    RestaurantFacade tests

--- app/poros/restaurant.rb
+++ app/poros/restaurant.rb
@@ -1,4 +1,6 @@
 class Restaurant
+  attr_reader :name, :address
+
   def initialize(data)
     @name = data[:businesses].first[:name]
     @address = data[:businesses].first[:location][:display_address].join(', ')
