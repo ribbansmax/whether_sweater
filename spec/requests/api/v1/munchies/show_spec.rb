7324c8458830812f3d54898c3e7f447976de7ab1
Max Ribbans <ribbans.max@gmail.com>
2021-03-08 11:09:30 -0500

    Add spec for munchies

---
+++ spec/requests/api/v1/munchies/show_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe "Munchies route" do
+  it "gets a destination city, travel time, name and address of restaurant serving the cuisine, that will be open." do
+    VCR.use_cassette("munchies") do
+      get '/api/v1/munchies?start=denver,co&destination=pueblo,co&food=hamburger'
+
+      expect(response).to be_successful
+    end
+  end
+end
