dc0aa621d717fcaee35f5f7ea0fa9a29f3efe8b2
Max Ribbans <ribbans.max@gmail.com>
2021-03-08 11:50:12 -0500

    Add current_weather for destination, using preworked facade/poro/service

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::MunchiesController < ApplicationController
   def show
     # begin
-      destination = MapQuestFacade.get_destination(params[:start], params[:destination])
+    destination = MapQuestFacade.get_destination(params[:start], params[:destination])
     # rescue
-
+    current_weather = ForecastFacade.get_forecast(destination).current_weather
     # end
     binding.pry
 
--- spec/requests/api/v1/munchies/show_spec.rb
+++ spec/requests/api/v1/munchies/show_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "Munchies route" do
   it "gets a destination city, travel time, name and address of restaurant serving the cuisine, that will be open." do
-    VCR.use_cassette("munchies") do
+    VCR.use_cassette("munchies_and_weather") do
       get '/api/v1/munchies?start=denver,co&destination=pueblo,co&food=hamburger'
 
       expect(response).to be_successful
