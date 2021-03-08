d91cdcb02d69b176c6d47b04c932fe664113f85a
Max Ribbans <ribbans.max@gmail.com>
2021-03-08 11:39:47 -0500

    Finish destination call, add facade test

--- app/poros/destination.rb
+++ app/poros/destination.rb
@@ -1,5 +1,9 @@
 class Destination
+  attr_reader :travel_time, :lat, :lon
+
   def initialize(data)
-    binding.pry
+    @travel_time = data[:route][:formattedTime]
+    @lat = data[:route][:locations].last[:latLng][:lat]
+    @lon = data[:route][:locations].last[:latLng][:lng]
   end
 end
