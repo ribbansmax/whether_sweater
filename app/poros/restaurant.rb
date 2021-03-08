843370af1f35b5508d76efabf211b754dd093e94
Max Ribbans <ribbans.max@gmail.com>
2021-03-08 12:30:05 -0500

    Stub time.now

--- app/poros/restaurant.rb
+++ app/poros/restaurant.rb
@@ -2,6 +2,5 @@ class Restaurant
   def initialize(data)
     @name = data[:businesses].first[:name]
     @address = data[:businesses].first[:location][:display_address].join(', ')
-    binding.pry
   end
 end
--- spec/requests/api/v1/munchies/show_spec.rb
+++ spec/requests/api/v1/munchies/show_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe "Munchies route" do
   it "gets a destination city, travel time, name and address of restaurant serving the cuisine, that will be open." do
     VCR.use_cassette("munchies_and_weather_and_yelp") do
+      stub_time = '2021-03-08 12:26:53 -0500'.to_time
+      allow(Time).to receive(:now).and_return(stub_time)
       get '/api/v1/munchies?start=denver,co&destination=pueblo,co&food=hamburger'
 
       expect(response).to be_successful
