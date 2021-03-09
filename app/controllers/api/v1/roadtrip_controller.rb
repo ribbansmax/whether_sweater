813df4afd15f1b3e4fb7bfa39bf4e885a0d617fe
Max Ribbans <ribbans.max@gmail.com>
2021-03-09 10:59:27 -0500

    add sad path for invalid api_key

--- app/controllers/api/v1/roadtrip_controller.rb
+++ app/controllers/api/v1/roadtrip_controller.rb
@@ -1,9 +1,15 @@
 class Api::V1::RoadtripController < ApplicationController
   def show
-    user = User.find_by(api_key: params[:api_key])
+    return invalid_key if !User.find_by(api_key: params[:api_key])
     destination = MapQuestFacade.get_destination(params[:origin], params[:destination])
     fforecast = ForecastFacade.get_future_forecast(destination)
     roadtrip = Roadtrip.new(params, destination, fforecast)
     render json: RoadtripSerializer.new(roadtrip)
+  end
+
+  protected
+
+  def invalid_key
+    render json: {"error" => 'api_key is invalid'}, status: 401
   end
 end
--- spec/requests/api/v1/roadtrip/show_spec.rb
+++ spec/requests/api/v1/roadtrip/show_spec.rb
@@ -34,4 +34,33 @@ describe "Makes a road trip" do
       end
     end
   end
+  describe "sad path" do
+    it "should no accept an invalid api key and return a 401" do
+      VCR.use_cassette('no_roadtrip_to_pueblo') do
+        data = {
+          email: 'email@example.com',
+          password: 'Password',
+          password_confirmation: 'Password'
+        }
+
+        user = User.create!(data)
+
+        headers = { "CONTENT_TYPE" => "application/json" }
+
+        data = {
+          origin: "Denver,CO",
+          destination: "Pueblo,CO",
+          api_key: '3'
+        }
+
+        post '/api/v1/road_trip', params: data
+
+        expect(response.status).to eq(401)
+
+        data = JSON.parse(response.body, symbolize_names: true)
+
+        expect(data[:error]).to eq('api_key is invalid')
+      end
+    end
+  end
 end
