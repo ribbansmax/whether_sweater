eb53b6da8409f1774226995ece5aa1341ca914f7
Max Ribbans <ribbans.max@gmail.com>
2021-03-09 14:21:27 -0500

    Get daily weather if trip > 47 hours

--- app/poros/future_forecast.rb
+++ app/poros/future_forecast.rb
@@ -3,9 +3,23 @@ class FutureForecast
 
   def initialize(data, hour)
     @id = nil
-    @weather_at_eta = {
-      temperature: data[:hourly][hour][:temp],
-      conditions: data[:hourly][hour][:weather].first[:description]
-    }
+    @weather_at_eta = find_weather(data, hour)
+  end
+
+  private
+
+  def find_weather(data, hour)
+    if hour <= 47
+      {
+        temperature: data[:hourly][hour][:temp],
+        conditions: data[:hourly][hour][:weather].first[:description]
+      }
+    else
+      day = hour/24
+      {
+        temperature: data[:daily][day][:temp][:day],
+        conditions: data[:daily][day][:weather].first[:description]
+      }
+    end
   end
 end
