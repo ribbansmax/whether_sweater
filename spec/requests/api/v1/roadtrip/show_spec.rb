7797d06f799f8db4ab584107fe8303dd20d6c6cd
Max Ribbans <ribbans.max@gmail.com>
2021-03-08 23:48:48 -0500

    Add road trip happy path

--- spec/requests/api/v1/roadtrip/show_spec.rb
+++ spec/requests/api/v1/roadtrip/show_spec.rb
@@ -3,19 +3,30 @@ require 'rails_helper'
 describe "Makes a road trip" do
   describe "happy path" do
     it "should sign in a user" do
-      data = {
-        email: 'email@example.com',
-        password: 'Password',
-        password_confirmation: 'Password'
-      }
+      VCR.use_cassette('roadtrip_to_pueblo') do
+        data = {
+          email: 'email@example.com',
+          password: 'Password',
+          password_confirmation: 'Password'
+        }
 
-      user = User.create!(data)
+        user = User.create!(data)
 
-      headers = { "CONTENT_TYPE" => "application/json" }
-      post '/api/v1/road_trip', params: data
+        headers = { "CONTENT_TYPE" => "application/json" }
 
-      expect(response.status).to eq(200)
+        data = {
+          origin: "Denver,CO",
+          destination: "Pueblo,CO",
+          api_key: user.api_key
+        }
 
+        post '/api/v1/road_trip', params: data
+
+        expect(response.status).to eq(200)
+
+        data = JSON.parse(response.body, symbolize_names: true)
+        binding.pry
+      end
     end
   end
 end
