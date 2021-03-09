861ca141c56ceeca314138991b2b59386e53af52
Max Ribbans <ribbans.max@gmail.com>
2021-03-08 19:39:57 -0500

    Add sad path bad password

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,8 +1,11 @@
 class Api::V1::SessionsController < ApplicationController
+
   def show
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       render json: UserSerializer.new(user)
+    else
+      render json: {'error' => 'Email and password do not match a user in our database'}, status: 401
     end
   end
 end
--- spec/requests/api/v1/sesisons/show_spec.rb
+++ spec/requests/api/v1/sesisons/show_spec.rb
@@ -21,4 +21,24 @@ describe "Signs in a user" do
       expect(data[:attributes][:api_key]).to eq(user.api_key)
     end
   end
+
+  describe 'sad path' do
+    it 'should return 401 if user is not authorized' do
+      data = {
+        email: 'email@example.com',
+        password: 'Password'
+      }
+
+      user = User.create!(data.merge(password_confirmation: 'Password'))
+
+      data[:password] = 'password'
+      headers = { "CONTENT_TYPE" => "application/json" }
+      post '/api/v1/sessions', params: data
+
+      expect(response.status).to eq(401)
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data[:error]).to eq('Email and password do not match a user in our database')
+    end
+  end
 end
