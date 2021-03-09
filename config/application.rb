f205c849eccbdeb647fbe3f286899ae8c3dc9b1f
Max Ribbans <ribbans.max@gmail.com>
2021-03-09 15:52:38 -0500

    add api rate limiting/minute

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ require 'action_view/railtie'
 require 'action_cable/engine'
 # require "sprockets/railtie"
 require 'rails/test_unit/railtie'
+require 'rack/throttle'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -19,6 +20,8 @@ Bundler.require(*Rails.groups)
 
 module WhetherSweater
   class Application < Rails::Application
+    config.middleware.use Rack::Throttle::Minute, max: ENV['RATE_LIMIT']
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
