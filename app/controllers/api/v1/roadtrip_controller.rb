4b6c4bf45bc6bf94f05426ccf6d5d972c012ae12
Max Ribbans <ribbans.max@gmail.com>
2021-03-08 23:49:47 -0500

    Add roadtrip serializer

--- app/controllers/api/v1/roadtrip_controller.rb
+++ app/controllers/api/v1/roadtrip_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::RoadtripController < ApplicationController
   def show
+    user = User.find_by(api_key: params[:api_key])
+    destination = MapQuestFacade.get_destination(params[:origin], params[:destination])
+    fforecast = ForecastFacade.get_future_forecast(destination)
+    roadtrip = Roadtrip.new(params, destination, fforecast)
+    render json: RoadtripSerializer.new(roadtrip)
   end
 end
--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -4,5 +4,10 @@ class ForecastFacade
       forecast = OpenWeatherApiService.forecast(location)
       forecast = Forecast.new(forecast)
     end
+
+    def get_future_forecast(location)
+      forecast = OpenWeatherApiService.forecast(location)
+      fforecast = FutureForecast.new(forecast, location.travel_time.to_i)
+    end
   end
 end
